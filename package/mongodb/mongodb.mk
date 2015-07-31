147e36463459a918b9ace89fb0ecc97b5d529612
codechaotic <thinkinfractals@gmail.com>
2015-07-30 21:35:53 -0400

    remove git hash

--- package/mongodb/mongodb.mk
+++ package/mongodb/mongodb.mk
@@ -1,6 +1,5 @@
 MONGODB_VERSION = r3.0.5
 MONGODB_SITE = git://github.com/mongodb/mongo
-MONGODB_COMMIT = 8bc4ae20708dbb493cb09338d9e7be6698e4a3a3
 MONGODB_SITE_METHOD = git
 
 MONGODB_DEPENDENCIES += host-scons host-python
@@ -10,7 +9,6 @@ MONGODB_BUILD_OPTS += --cxx=$(TARGET_CXX)
 MONGODB_BUILD_OPTS += --ld=$(TARGET_CXX)
 MONGODB_BUILD_OPTS += --libpath=$(STAGING_DIR)
 MONGODB_BUILD_OPTS += --variant-dir=variant
-MONGODB_BUILD_OPTS += MONGO_GIT_HASH=$(MONGODB_COMMIT)
 
 ifeq ($(BR2_PACKAGE_MONGODB_SSL),y)
 MONGODB_BUILD_OPTS += --ssl
@@ -34,12 +32,8 @@ MONGODB_TARGETS += build/install/bin/mongoperf
 endif
 
 define MONGODB_BUILD_CMDS
-			 $(TARGET_CC) -v
 			 $(SCONS) $(MONGODB_TARGETS) $(MONGODB_BUILD_OPTS) -C $(@D)
 endef
-
-#$(info $$STAGING_DIR is [$(STAGING_DIR)])
-#$(error Aborting!)
 
 define MONGODB_INSTALL_TARGET_CMDS
 				mkdir -p $(TARGET_DIR)/usr/local; \
